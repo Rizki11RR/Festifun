4575d727285b8956c2366834de40c718d8539ff4
karlen <karlenderawan07@gmail.com>
2023-11-21 03:07:52 +0800

    q

--- src/pages/Event/main.jsx
+++ src/pages/Event/main.jsx
@@ -1,16 +1,29 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React from 'react';
+import Carousel from './Carousel';
+import SearchBar from './SearchBar';
+import Lay from './Lay';
 
 
 
 function Event() {
   return (
+<div>
     <div >
       <h1>Event</h1>
 
     </div>
     
-  )
+
+    <div>
+      <Carousel />
+      <br />
+      <SearchBar />
+      <br />
+      <Lay />
+    </div>
+    </div>
+  );
+
 }
 
-export default Event
+export default Event;
