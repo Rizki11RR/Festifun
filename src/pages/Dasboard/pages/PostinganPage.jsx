32f0ce8e36b94bdf72dc05f9053054705db58a9c
Karlen404 <karlenderawan07@gmail.com>
2023-12-07 18:13:18 +0800

    fix tiketku

--- src/pages/Dasboard/pages/PostinganPage.jsx
+++ src/pages/Dasboard/pages/PostinganPage.jsx
@@ -1,66 +1,39 @@
 // PostinganPage.jsx
 import React from "react";
 import Profil from "../components/Profil/Profil";
+import CardComponent from "../../../components/molecules/Card";
+import { amikom } from "../../../assets/main";
 
 function PostinganPage() {
     return (
         <div className="container-fluid">
             <Profil/>
             <div className="row">
-                <div className="col-sm-4">
-                    <div className="card m-2">
-                        <img src="https://infogresik.id/wp-content/uploads/2022/11/Konser-denny-caknan-di-wisata-setigi-gresik.jpg" className="card-img-top"/>
-                        <div className="card-body">
-                        <h5 className="card-title fw-bold">Ambyar Party 2022</h5>
-                        <p className="card-text text-primary">Rp 75.000</p>
-                        </div>
-                    </div>
+                <div className="col-sm-4 mb-4">
+                    <CardComponent image={amikom} title="AMIKOM FEST 2023 BE YOUNG FOREVER" price='50.000'/>
                 </div>
-                <div className="col-sm-4">
-                    <div className="card m-2">
-                        <img src="https://infogresik.id/wp-content/uploads/2022/11/Konser-denny-caknan-di-wisata-setigi-gresik.jpg" className="card-img-top"/>
-                        <div className="card-body">
-                        <h5 className="card-title fw-bold">Ambyar Party 2022</h5>
-                        <p className="card-text text-primary">Rp 75.000</p>
-                        </div>
-                    </div>
+
+                <div className="col-sm-4 mb-4">
+                    <CardComponent image={amikom} title="AMIKOM FEST 2023 BE YOUNG FOREVER" price='50.000'/>
                 </div>
-                <div className="col-sm-4">
-                    <div className="card m-2">
-                        <img src="https://infogresik.id/wp-content/uploads/2022/11/Konser-denny-caknan-di-wisata-setigi-gresik.jpg" className="card-img-top"/>
-                        <div className="card-body">
-                        <h5 className="card-title fw-bold">Ambyar Party 2022</h5>
-                        <p className="card-text text-primary">Rp 75.000</p>
-                        </div>
-                    </div>
+
+                <div className="col-sm-4 mb-4">
+                    <CardComponent image={amikom} title="AMIKOM FEST 2023 BE YOUNG FOREVER" price='50.000'/>
                 </div>
-                <div className="col-sm-4">
-                    <div className="card m-2">
-                        <img src="https://infogresik.id/wp-content/uploads/2022/11/Konser-denny-caknan-di-wisata-setigi-gresik.jpg" className="card-img-top"/>
-                        <div className="card-body">
-                        <h5 className="card-title fw-bold">Ambyar Party 2022</h5>
-                        <p className="card-text text-primary">Rp 75.000</p>
-                        </div>
-                    </div>
+
+                <div className="col-sm-4 mb-4">
+                    <CardComponent image={amikom} title="AMIKOM FEST 2023 BE YOUNG FOREVER" price='50.000'/>
                 </div>
-                <div className="col-sm-4">
-                    <div className="card m-2">
-                        <img src="https://infogresik.id/wp-content/uploads/2022/11/Konser-denny-caknan-di-wisata-setigi-gresik.jpg" className="card-img-top"/>
-                        <div className="card-body">
-                        <h5 className="card-title fw-bold">Ambyar Party 2022</h5>
-                        <p className="card-text text-primary">Rp 75.000</p>
-                        </div>
-                    </div>
+
+                <div className="col-sm-4 mb-4">
+                    <CardComponent image={amikom} title="AMIKOM FEST 2023 BE YOUNG FOREVER" price='50.000'/>
                 </div>
-                <div className="col-sm-4">
-                    <div className="card m-2">
-                        <img src="https://infogresik.id/wp-content/uploads/2022/11/Konser-denny-caknan-di-wisata-setigi-gresik.jpg" className="card-img-top"/>
-                        <div className="card-body">
-                        <h5 className="card-title fw-bold">Ambyar Party 2022</h5>
-                        <p className="card-text text-primary">Rp 75.000</p>
-                        </div>
-                    </div>
+
+                <div className="col-sm-4 mb-4">
+                    <CardComponent image={amikom} title="AMIKOM FEST 2023 BE YOUNG FOREVER" price='50.000'/>
                 </div>
+                
+               
             </div>
         </div>
     );
